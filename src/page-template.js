5a906117b5958e8ee9623cbec9438a66fc14f902
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-06 19:05:07 -0500

    corrected bootstrap tags for background and text align for header

--- src/page-template.js
+++ src/page-template.js
@@ -39,11 +39,11 @@ function render(teamMembersArr) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Work Team</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel='stylesheet' href='./output/style.css'>
+        <link rel='stylesheet' href='style.css'>
     </head>
     <body>
         <header>
-            <h1>My Team</h1>
+            <h1 class='bg-primary text-center'>My Team</h1>
         </header>
             <section>
             <div class="container">
